95ef8e7e99d63abfc61f64f5cbae5085c066a205
DeshiniDissanayake <68861131+DeshiniDissanayake@users.noreply.github.com>
2021-09-08 21:51:53 +0530

    Test class for Food Inventory
    Test class for Food Inventory

---
+++ FoodInventoryNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodInventoryNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodInventoryNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
