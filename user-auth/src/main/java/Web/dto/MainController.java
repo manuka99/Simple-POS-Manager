34b7a4b11619e60821f6d16260b15f6d7930edce
Rane1998 <54766167+Rane1998@users.noreply.github.com>
2021-09-10 09:38:15 +0530

    User auth updated

---
+++ user-auth/src/main/java/Web/dto/MainController.java
@@ -0,0 +1,17 @@
+package Web.dto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+}
