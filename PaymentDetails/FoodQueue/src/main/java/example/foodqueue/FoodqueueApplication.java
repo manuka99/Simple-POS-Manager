291808c91689b71650620c2129f556e5cfeb9fac
DeshiniDissanayake <68861131+DeshiniDissanayake@users.noreply.github.com>
2021-10-01 22:12:13 +0530

    Add files via upload

---
+++ PaymentDetails/FoodQueue/src/main/java/example/foodqueue/FoodqueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodqueueApplication.class, args);
+	}
+
+}
